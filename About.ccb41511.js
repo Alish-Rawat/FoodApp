08dbbc806149666242ff1a0c9ab3bb0e5f086145
Alish-Rawat <rawatalish4@gmail.com>
2023-12-17 04:10:52 +0530

    Updates

---
+++ About.ccb41511.js
@@ -0,0 +1,15 @@
+function e(e){return e&&e.__esModule?e.default:e}var n=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire40f1;(0,n.register)("3yXQA",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:function(){return u},set:void 0,enumerable:!0,configurable:!0});var o=n("5aRSq");n("kKmmy");var i=n("2I9iM"),l=n("4Iuzv"),s=n("fmRNI");// const About2 = () => {
+//   return (
+//     <div>
+//       <h1>About this page</h1>
+//       <p>This is for Practice</p>
+//       {/* <Outlet /> */}
+//       <ProfileClass name="alish" />
+//       <ProfileFuntional name={"ALish"} />
+//     </div>
+//   );
+// };  // Functional Component
+class d extends /*@__PURE__*/e(l).Component{constructor(e){super(e)}componentDidMount(){// API call
+}render(){return/*#__PURE__*/(0,o.jsxs)("div",{children:[/*#__PURE__*/(0,o.jsx)("h1",{children:"About this page"}),/*#__PURE__*/(0,o.jsx)(s.default.Consumer,{children:({user:e})=>/*#__PURE__*/(0,o.jsxs)("h4",{className:"font-bold text-xl p-10",children:[e.name," - ",e.email]})}),/*#__PURE__*/(0,o.jsx)("p",{children:"This is for Practice"}),/*#__PURE__*/(0,o.jsx)(i.default,{name:"ALish"})]})}}var u=d});//# sourceMappingURL=About.ccb41511.js.map
+
+//# sourceMappingURL=About.ccb41511.js.map
